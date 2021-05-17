4220f8ebd762fa04611e56c7fdb70a6e8bda97db
msedalatzadeh <msedalatzadeh@math.tu-chemnitz.de>
2021-05-17 11:11:25 -0400

    notebooks added

---
+++ k10_NLP.py
@@ -0,0 +1,26 @@
+import nltk
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+                
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
+
+
+def get_sec_data(cik, doc_type, start=0, count=60):
+    rss_url = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany' \
+        '&CIK={}&type={}&start={}&count={}&owner=exclude&output=atom' \
+        .format(cik, doc_type, start, count)
+    sec_data = sec_api.get(rss_url)
+    feed = BeautifulSoup(sec_data.encode('ascii'), 'xml').feed
+    entries = [
+        (
+            entry.content.find('filing-href').getText(),
+            entry.content.find('filing-type').getText(),
+            entry.content.find('filing-date').getText())
+        for entry in feed.find_all('entry', recursive=False)]
+
+    return entries
